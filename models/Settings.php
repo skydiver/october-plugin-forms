dd46546b058003b7bccc6b10524c90796327e2b0
Martin <skydiver@users.noreply.github.com>
2017-01-07 00:23:14 -0300

    disabled unused code

--- models/Settings.php
+++ models/Settings.php
@@ -12,14 +12,13 @@
         public $settingsCode   = 'martin_forms_settings';
         public $settingsFields = 'fields.yaml';
 
-
-        public function __construct() {
-            $this->attributes = [
-                'global_messages_success' => Lang::get('martin.forms::lang.settings.global_messages_success.default'),
-                'global_messages_errors'  => Lang::get('martin.forms::lang.settings.global_messages_errors.default'),
-            ];
-            parent::__construct();
-        }
+        // public function __construct() {
+        //     $this->attributes = [
+        //         'global_messages_success' => Lang::get('martin.forms::lang.settings.global_messages_success.default'),
+        //         'global_messages_errors'  => Lang::get('martin.forms::lang.settings.global_messages_errors.default'),
+        //     ];
+        //     parent::__construct();
+        // }
 
     }
 
