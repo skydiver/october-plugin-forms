6743a18b190bd2388b3822bdc2fe055b94efc203
Martin <skydiver@users.noreply.github.com>
2017-01-12 01:53:34 -0300

    Export Records
    * added export filters
    * added export settings

--- controllers/Exports.php
+++ controllers/Exports.php
@@ -29,16 +29,60 @@
         }
 
         public function csv() {
-            $records = Record::get()->toArray();
+
+            $records = Record::orderBy('created_at');
+
+            # FILTER GROUPS
+            if(!empty($groups = post('Record.filter_groups'))) {
+                $records->whereIn('group', $groups);
+            }
+
+            # FILTER DATE
+            if(!empty($date_after = post('Record.filter_date_after'))) {
+                $records->whereDate('created_at', '>=', $date_after);
+            }
+
+            # FILTER DATE
+            if(!empty($date_before = post('Record.filter_date_before'))) {
+                $records->whereDate('created_at', '<=', $date_before);
+            }
+
+            # FILTER DELETED
+            if($deleted = post('Record.options_deleted')) {
+                $records->withTrashed();
+            }
+
+            # CREATE CSV
             $csv = CsvWriter::createFromFileObject(new SplTempFileObject());
-            $csv->insertOne([e(trans('martin.forms::lang.controllers.records.columns.id')), e(trans('martin.forms::lang.controllers.records.columns.ip')), e(trans('martin.forms::lang.controllers.records.columns.created_at')), e(trans('martin.forms::lang.controllers.records.columns.form_data')) . " >>>"]);
-            foreach($records as $row) {
+
+            # CSV HEADERS
+            $headers = [e(trans('martin.forms::lang.controllers.records.columns.form_data')) . " >>>"];
+
+            # METADATA HEADERS
+            if($metadata = post('Record.options_metadata')) {
+                $meta_headers = [
+                    e(trans('martin.forms::lang.controllers.records.columns.id')),
+                    e(trans('martin.forms::lang.controllers.records.columns.group')),
+                    e(trans('martin.forms::lang.controllers.records.columns.ip')),
+                    e(trans('martin.forms::lang.controllers.records.columns.created_at')),
+                ];
+                $headers = array_merge($meta_headers, $headers);
+            }
+
+            # ADD HEADERS
+            $csv->insertOne($headers);
+
+            # WRITE CSV LINES
+            foreach($records->get()->toArray() as $row) {
                 $data = (array) json_decode($row['form_data']);
-                array_unshift($data, $row['id'], $row['ip'], $row['created_at']);
+                if($metadata = post('Record.options_metadata')) { array_unshift($data, $row['id'], $row['group'], $row['ip'], $row['created_at']); }
                 $csv->insertOne($data);
             }
+
+            # RETURN CSV
             $csv->output('records.csv');
             exit();
+
         }
 
     }
