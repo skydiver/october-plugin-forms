124e756f680ebed538f922708d50b21edef1d50c
Martin <skydiver@users.noreply.github.com>
2017-03-30 23:48:00 -0300

    * fixed inline errors not showing * added new property to handle inline errors

--- assets/js/inline-errors.js
+++ assets/js/inline-errors.js
@@ -2,10 +2,12 @@ $(document).on('ajaxError', function(event, context) {
     if(context.responseJSON !== undefined) {
         var fields = context.responseJSON.error_fields;
         $.each(fields, function(index, value) {
-            var div = $("div[data-validate-for='" + index + "']").text(value).closest('.form-group').addClass('has-error');
+            $("div[data-validate-for='" + index + "']").
+                text(value).show().
+                closest('.form-group').addClass('has-error');
         });
     }
-})
+});
 
 $(document).on('ajaxPromise', '[data-request]', function() {
     $(this).closest('form').find('.form-group.has-error').removeClass('has-error');
