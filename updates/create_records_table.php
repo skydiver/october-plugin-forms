40c1a0678de9d22fafaa80c9fe4e3261c77a1825
Martin <skydiver@users.noreply.github.com>
2017-11-27 08:40:56 -0300

    1.4.6.1: changed database field from json to text to support MySQL 5.5

--- updates/create_records_table.php
+++ updates/create_records_table.php
@@ -1,28 +1,28 @@
 <?php
 
-    namespace Martin\Forms\Updates;
+namespace Martin\Forms\Updates;
 
-    use Schema;
-    use October\Rain\Database\Schema\Blueprint;
-    use October\Rain\Database\Updates\Migration;
+use Schema;
+use October\Rain\Database\Schema\Blueprint;
+use October\Rain\Database\Updates\Migration;
 
-    class CreateRecordsTable extends Migration {
+class CreateRecordsTable extends Migration {
 
-        public function up() {
-            Schema::create('martin_forms_records', function(Blueprint $table) {
-                $table->engine = 'InnoDB';
-                $table->increments('id');
-                $table->json('form_data')->nullable();
-                $table->string('ip')->nullable();
-                $table->softDeletes();
-                $table->timestamps();
-            });
-        }
-
-        public function down() {
-            Schema::dropIfExists('martin_forms_records');
-        }
-
+    public function up() {
+        Schema::create('martin_forms_records', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->text('form_data')->nullable();
+            $table->string('ip')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
     }
+
+    public function down() {
+        Schema::dropIfExists('martin_forms_records');
+    }
+
+}
 
 ?>
